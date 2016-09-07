ea04b778b8388d85a7d861d77325d9c9f411f80a
Pablo Acuña <pabloacuna88@gmail.com>
2016-09-06 21:09:49 -0300

    ADD: method for creating table for a month and a year

--- lib/partitionable/acts_as_partitionable.rb
+++ lib/partitionable/acts_as_partitionable.rb
@@ -12,6 +12,22 @@ module Partitionable
           formatted_month = sprintf('%02d', month)
           "#{self.table_name}_y#{year}m#{formatted_month}"
         end
+
+        def create_table_for(month, year)
+          table = partition_table_name(month, year)
+          index_name = "#{table}_logdate_site"
+          first_day_of_month = Date.civil(year, month, 1)
+          first_day_next_month = (first_day_of_month + 1.month)
+
+          ActiveRecord::Base.connection.execute(
+            <<-SQL
+          CREATE TABLE #{table} (
+              CHECK ( logdate >= DATE '#{first_day_of_month.to_s}' AND logdate < DATE '#{first_day_next_month.to_s}' )
+          ) INHERITS (#{self.table_name});
+          CREATE INDEX #{index_name} ON #{table} (site, token);
+            SQL
+          )
+        end
       end
     end
   end
--- test/acts_as_partitionable_test.rb
+++ test/acts_as_partitionable_test.rb
@@ -4,4 +4,9 @@ class ActsAsPartitionableTest < ActiveSupport::TestCase
   def test_article_stats_partition_table_name
     assert_equal "article_stats_y2015m01", ArticleStat.partition_table_name(1,2015)
   end
+
+  def test_article_stats_create_table_for
+    ArticleStat.create_table_for(1,2015)
+    assert_equal true, ActiveRecord::Base.connection.data_source_exists?("article_stats_y2015m01")
+  end
 end
